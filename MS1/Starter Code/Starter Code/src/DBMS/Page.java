11e40ce6f8eff9bda7be571e0e3a052dde936e92
alyashraf184 <alyashraf2005@yahoo.com>
2026-04-17 13:01:15 +0200

    Reverted All Changes

--- MS1/Starter Code/Starter Code/src/DBMS/Page.java
+++ MS1/Starter Code/Starter Code/src/DBMS/Page.java
@@ -3,36 +3,7 @@ package DBMS;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Page implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    private ArrayList<String[]> rows;
-
-    public Page() {
-        rows = new ArrayList<String[]>();
-    }
-
-    public void addRecord(String[] record) {
-        rows.add(record);
-    }
-
-    public String[] getRecord(int index) {
-        if (index < 0 || index >= rows.size()) {
-            return null;
-        }
-        return rows.get(index);
-    }
-
-    public ArrayList<String[]> getRows() {
-        return rows;
-    }
-
-    public int size() {
-        return rows.size();
-    }
-
-    public boolean isFull(int pageSize) {
-        return rows.size() >= pageSize;
-    }
+public class Page implements Serializable
+{
+	
 }
